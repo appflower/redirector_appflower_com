be47a64a7a65727084da3b4cc166f9ee29286756
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-11-23 12:04:48 +0100

    typo fix

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 $url = 'www.appflower.com';
 switch ($_SERVER['HTTP_HOST']) {
-    case 'signup.appflower.com: $url = 'beta.appflower.com/cms/demoRegistration'; break;
+    case 'signup.appflower.com': $url = 'beta.appflower.com/cms/demoRegistration'; break;
 }
 header("Location: http://$url");
 exit;
